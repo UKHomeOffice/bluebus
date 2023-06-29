41189e8a5a7429b91d8a25849809f0e5f1f18b1b
Rich Birch <ringo@albumsnaps.com>
2023-06-29 11:19:55 +0100

    Throw exception on non-200 response status

--- src/main/scala/bluebus/client/ServiceBusClient.scala
+++ src/main/scala/bluebus/client/ServiceBusClient.scala
@@ -3,13 +3,12 @@ package bluebus.client
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.headers.RawHeader
-import akka.http.scaladsl.model.{HttpHeader, HttpMethods, HttpRequest}
+import akka.http.scaladsl.model.{HttpHeader, HttpMethods, HttpRequest, HttpResponse}
 import bluebus.configuration.SBusConfig
-
-import scala.concurrent.duration.DurationInt
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
 
 class ServiceBusClient(config: SBusConfig)(implicit system: ActorSystem) {
   private val readHeadersRaw = Map(
@@ -20,13 +19,24 @@ class ServiceBusClient(config: SBusConfig)(implicit system: ActorSystem) {
   private def constructHeaders(readHeadersRaw: Map[String, String]): Seq[HttpHeader] =
     readHeadersRaw.map { case (k, v) => RawHeader(k, v) }.toSeq
 
-  def peek: Future[String] = Http()
-    .singleRequest(HttpRequest(uri = s"${config.endpoint}/head", method = HttpMethods.POST, headers = constructHeaders(readHeadersRaw)))
-    .flatMap(_.entity.toStrict(10.seconds).map(_.data.utf8String))
+  def peek: Future[String] = filter200(
+    Http().singleRequest(HttpRequest(uri = s"${config.endpoint}/head", method = HttpMethods.POST, headers = constructHeaders(readHeadersRaw)))
+  )
 
-  def receive: Future[String] = Http()
-    .singleRequest(HttpRequest(uri = s"${config.endpoint}/head", method = HttpMethods.DELETE, headers = constructHeaders(readHeadersRaw)))
-    .flatMap(_.entity.toStrict(10.seconds).map(_.data.utf8String))
+  private def filter200(eventualResponse: Future[HttpResponse]) = {
+    eventualResponse
+      .flatMap {
+        case r if r.status.isSuccess() =>
+          r.entity.toStrict(10.seconds).map(_.data.utf8String)
+        case r =>
+          Future.failed(new Exception(s"Non-200 response: ${r.status}"))
+      }
+  }
+
+  def receive: Future[String] = filter200(
+    Http()
+      .singleRequest(HttpRequest(uri = s"${config.endpoint}/head", method = HttpMethods.DELETE, headers = constructHeaders(readHeadersRaw)))
+  )
 
   def send(message: String, messageId: String): Future[String] = {
     val headers = Map(
@@ -34,8 +44,8 @@ class ServiceBusClient(config: SBusConfig)(implicit system: ActorSystem) {
       "Authorization" -> config.sasToken,
       "Content-Type" -> config.contentType)
 
-    Http()
-      .singleRequest(HttpRequest(uri = config.endpoint, headers = constructHeaders(headers)))
-      .flatMap(_.entity.toStrict(10.seconds).map(_.data.utf8String))
+    filter200(
+      Http().singleRequest(HttpRequest(uri = config.endpoint, headers = constructHeaders(headers)))
+    )
   }
 }
